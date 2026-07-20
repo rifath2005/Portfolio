98f9baa2d83f0fe9022e34c90c359d3b8e190500
sword <babsword3@gmail.com>
2026-07-20 12:00:50 +0530

    fix: add missing useEffect import in ProjectScene

--- components/ProjectScene.tsx
+++ components/ProjectScene.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useRef, useMemo, useState } from "react";
+import { useRef, useMemo, useState, useEffect } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import * as THREE from "three";
 
